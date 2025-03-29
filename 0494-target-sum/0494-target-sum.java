55c8e1b6a6871a9d97a1acaa2af73c9ee979d8fa
Manan Malik <135524466+Mananalik@users.noreply.github.com>
2025-03-29 22:05:24 +0530

    Time: 4 ms (93.6%), Space: 43.1 MB (52.33%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -12,10 +12,47 @@ class Solution {
         //         nextDp.put(sum-num,nextDp.getOrDefault(sum-num,0)+count);
         //     }
         //     dp = nextDp;
-        // }
         
-        // return dp.getOrDefault(target,0);
-        int n  = nums.length;
+        // }
+         // return dp.getOrDefault(target,0);
+
+
+        // //Tabulation
+        //     int n  = nums.length;
+        // int totalSum = 0;
+        // for(int i=0;i<n;i++){
+        //     totalSum+=nums[i];
+        // }
+        // if(totalSum-target<0) return 0;
+        // if((totalSum-target)%2==1) return 0;
+        // int T = (totalSum-target)/2;
+        // int dp[][]=  new int[n][T+1];
+        // if(nums[0]==0){
+        //     dp[0][0] = 2;
+        // }else{
+        //     dp[0][0] = 1;
+        // }
+        // if(nums[0]!=0 && nums[0]<=T){
+        //     dp[0][nums[0]]= 1;
+        // }
+        // for(int index = 1;index<n;index++){
+        //     for(int prey = 0;prey<=T;prey++){
+        //         int notPick = dp[index-1][prey];
+        //         int pick = 0;
+        //         if(nums[index]<=prey){
+        //             pick = dp[index-1][prey-nums[index]];
+        //         }
+        //         dp[index][prey] = pick+notPick;
+        //     }
+
+        // }
+        // return dp[n-1][T];
+
+
+
+        //-------->TABULATION WITH SPACE OPTIMIZATION-------------->
+
+         int n  = nums.length;
         int totalSum = 0;
         for(int i=0;i<n;i++){
             totalSum+=nums[i];
@@ -23,29 +60,63 @@ class Solution {
         if(totalSum-target<0) return 0;
         if((totalSum-target)%2==1) return 0;
         int T = (totalSum-target)/2;
-        int dp[][]=  new int[n][T+1];
-        for(int x[]:dp){
-            Arrays.fill(x,-1);
+        int prev[]=  new int[T+1];
+        int curr[]  = new int[T+1];
+        if(nums[0]==0){
+            prev[0] = 2;
+        }else{
+            prev[0] = 1;
         }
-       return func(n-1,T,nums,dp);
+        if(nums[0]!=0 && nums[0]<=T){
+            prev[nums[0]]= 1;
+        }
+        for(int index = 1;index<n;index++){
+            for(int prey = 0;prey<=T;prey++){
+                int notPick = prev[prey];
+                int pick = 0;
+                if(nums[index]<=prey){
+                    pick = prev[prey-nums[index]];
+                }
+                curr[prey] = pick+notPick;
+            }
+            prev = curr.clone();
+        }
+        return prev[T];
+    //     int n  = nums.length;
+    //     int totalSum = 0;
+    //     for(int i=0;i<n;i++){
+    //         totalSum+=nums[i];
+    //     }
+    //     if(totalSum-target<0) return 0;
+    //     if((totalSum-target)%2==1) return 0;
+    //     int T = (totalSum-target)/2;
+    //     int dp[][]=  new int[n][T+1];
+    //     for(int x[]:dp){
+    //         Arrays.fill(x,-1);
+    //     }
+    //    return func(n-1,T,nums,dp);
 
     }
-    static int func(int index , int target , int [] arr , int[][] dp){
-        if(index==0){
-            if(target==0 && arr[0]==0){
-                return 2;
-            }
-            if(target==0 || target==arr[0]){
-                return 1;
-            }
-            return 0;
-        }
-        if(dp[index][target]!=-1) return dp[index][target];
-        int notPick = func(index-1,target,arr,dp);
-        int pick = 0;
-        if(arr[index]<=target){
-            pick = func(index-1,target-arr[index],arr,dp);
-        }
-        return dp[index][target] = pick+notPick;
-    }
+
+    // MEMOIZATION
+
+
+    // static int func(int index , int target , int [] arr , int[][] dp){
+    //     if(index==0){
+    //         if(target==0 && arr[0]==0){
+    //             return 2;
+    //         }
+    //         if(target==0 || target==arr[0]){
+    //             return 1;
+    //         }
+    //         return 0;
+    //     }
+    //     if(dp[index][target]!=-1) return dp[index][target];
+    //     int notPick = func(index-1,target,arr,dp);
+    //     int pick = 0;
+    //     if(arr[index]<=target){
+    //         pick = func(index-1,target-arr[index],arr,dp);
+    //     }
+    //     return dp[index][target] = pick+notPick;
+    // }
 }
