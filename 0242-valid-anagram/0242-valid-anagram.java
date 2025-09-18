eb069bd2497660957dbd07cf3cfd14014e4c8f15
Manan Malik <135524466+Mananalik@users.noreply.github.com>
2025-09-18 16:22:43 +0530

    Time: 4 ms (76.53%), Space: 43.1 MB (76.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,29 +1,36 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character,Integer> mp = new HashMap<>();
-        int n = s.length();
-        int m = t.length();
-        int count = 0;
-        if(m!=n){
-            return false;
-        }
-        for(int i=0;i<n;i++){
-            if(!mp.containsKey(s.charAt(i))){
-                mp.put(s.charAt(i),1);
-            }else{
-                mp.put(s.charAt(i),mp.get(s.charAt(i))+1);
-            }
-        }
-        for(int i=0;i<n;i++){
-            if(mp.containsKey(t.charAt(i))){
-                mp.put(t.charAt(i),mp.get(t.charAt(i))-1);
-            }
-        }
-        for(Map.Entry<Character,Integer> entry:mp.entrySet()){
-            if(entry.getValue()!=0){
-                count++;
-            }
-        }
-        return count==0?true:false;
+        // HashMap<Character,Integer> mp = new HashMap<>();
+        // int n = s.length();
+        // int m = t.length();
+        // int count = 0;
+        // if(m!=n){
+        //     return false;
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(!mp.containsKey(s.charAt(i))){
+        //         mp.put(s.charAt(i),1);
+        //     }else{
+        //         mp.put(s.charAt(i),mp.get(s.charAt(i))+1);
+        //     }
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(mp.containsKey(t.charAt(i))){
+        //         mp.put(t.charAt(i),mp.get(t.charAt(i))-1);
+        //     }
+        // }
+        // for(Map.Entry<Character,Integer> entry:mp.entrySet()){
+        //     if(entry.getValue()!=0){
+        //         count++;
+        //     }
+        // }
+        // return count==0?true:false;
+
+        //2nd method
+        if(s.length()!=t.length()) return false;
+        int freq[] = new int[26];
+        for(int i=0;i<s.length();i++) freq[s.charAt(i)-'a']++;
+        for(int j=0;j<t.length();j++) if (--freq[t.charAt(j)-'a']<0) return false;
+        return true;
     }
 }
