41ba8e888cee3f91116bd9e8df5abed2d5b998d0
Manan Malik <135524466+Mananalik@users.noreply.github.com>
2025-09-23 18:23:43 +0530

    Time: 1 ms (6.5%), Space: 41.4 MB (93.59%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -13,19 +13,52 @@
  *     }
  * }
  */
+class Node{
+    int val;
+    Node next;
+    Node(int val){
+        this.val = val;
+        this.next = null;
+    }
+}
+class MyListNode{
+    private Node head;
+    public MyListNode(){
+        head = null;
+    }
+    public void addNew(int val){
+        Node newNode = new Node(val);
+        newNode.next = head;
+        head = newNode;
+    }
+    public List<Integer> toList(){
+        List<Integer> result = new ArrayList<>();
+        Node curr = head;
+        while(curr!=null){
+            result.add(curr.val);
+            curr = curr.next;
+        }
+        return result;
+    }
+}
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        postOrder(list,root);
-        return list;
+        MyListNode ans = new MyListNode();
+        Stack<TreeNode> st = new Stack<>();
+        if(root==null) return ans.toList();
+        st.push(root);
+
+        while(!st.isEmpty()){
+            TreeNode curr = st.pop();
+            ans.addNew(curr.val);
+            if(curr.left!=null){
+                st.add(curr.left);
+            }
+            if(curr.right!=null){
+                st.add(curr.right);
+            }
+        }
+        return ans.toList();
     }
-    static List<Integer> postOrder(List<Integer> list,TreeNode root){
-      if(root==null){
-        return list;
-      }
-      postOrder(list,root.left);
-      postOrder(list,root.right);
-      list.add(root.val);
-      return list;
-    }
+    
 }
