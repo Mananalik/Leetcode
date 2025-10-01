a2cc00a4c43fd5335e5219aeff3a4c7cb712c494
Manan Malik <135524466+Mananalik@users.noreply.github.com>
2025-10-01 07:38:57 +0530

    Time: 1 ms (63.26%), Space: 41.8 MB (98.52%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,35 +15,35 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> arr = new ArrayList<>();
-        if(root == null){
-            return arr;
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root==null){
+            return ans;
         }
+
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-        boolean lefttoright = true;
+        boolean isLeft = true;
         while(!q.isEmpty()){
-           
-            int n = q.size();
-            List<Integer> arr1 = new ArrayList<>();
-            for(int i=0;i<n;i++){
-                 TreeNode node = q.poll();
-             if(lefttoright){
-                arr1.add(node.val);
-             }else{
-                arr1.add(0,node.val);
-             }
-             if(node.left!=null){
-                q.add(node.left);
-             }
-             if(node.right!=null){
-                q.add(node.right);
-             }
+            int size = q.size();
+            List<Integer> list = new ArrayList<>();
+            for(int i=0;i<size;i++){
+                TreeNode node = q.poll();
+                if(isLeft){
+                    list.add(node.val);
+                }
+                if(!isLeft){
+                    list.add(0,node.val);
+                }
+                if(node.left!=null){
+                    q.add(node.left);
+                }
+                if(node.right!=null){
+                    q.add(node.right);
+                }
             }
-            lefttoright = !lefttoright;
-            arr.add(arr1);
+            isLeft = !isLeft;
+            ans.add(list);
         }
-         return arr;
+        return ans;
     }
-   
 }
