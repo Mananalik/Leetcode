2a17de98fa88f33e12f7f7443fde0026407b355b
Manan Malik <135524466+Mananalik@users.noreply.github.com>
2026-02-22 17:59:16 +0530

    Time: 85 ms (28.83%), Space: 76 MB (15.38%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,33 +1,26 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int sum  = 0;
         int n = nums.length;
-      
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-        }
-        if(sum%2!=0){
-            return false;
-        }
+        int sum=0;
+        for(int i: nums) sum+=i;
+        if(sum%2!=0) return false;
         int target = sum/2;
-          int dp[][] = new int[n][target+1];
-          for(int x[]: dp){
-            Arrays.fill(x,-1);
-          }
-        return func(n-1, target,nums,dp);
+        int dp[][] = new int[n][target+1];
+        for(int i[]: dp){
+            Arrays.fill(i,-1);
+        }
+        return func(n-1,target,nums,dp);
     }
-    static boolean func(int index , int target , int arr[], int dp[][]){
+    public boolean func(int index, int target, int [] arr, int [][] dp){
         if(target==0) return true;
         if(index==0) return arr[index]==target;
-        if(dp[index][target]!=-1){
-            return dp[index][target]==0?false:true;
+        if(dp[index][target]!=-1) return dp[index][target]==1;
+        boolean notTaken = func(index-1,target,arr,dp);
+        boolean taken = false;
+        if(target>=arr[index]){
+            taken = func(index-1,target-arr[index],arr,dp);
         }
-        boolean notPick = func(index-1, target, arr,dp);
-        boolean pick = false;
-        if(arr[index]<=target){
-            pick = func(index-1, target-arr[index], arr,dp);
-        }
-        dp[index][target] = pick || notPick ? 1:0;
-        return pick || notPick;
+        dp[index][target] = (taken || notTaken)?1:0;
+        return taken|| notTaken;
     }
 }
