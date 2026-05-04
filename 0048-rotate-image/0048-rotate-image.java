c8202578d0bda2cd1524375d5d96dd97ce35fb67
Manan Malik <135524466+Mananalik@users.noreply.github.com>
2026-05-04 17:17:34 +0530

    Time: 0 ms (100%), Space: 44 MB (16.04%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -2,29 +2,32 @@ class Solution {
     public void rotate(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = i; j < matrix[0].length; j++) {
-                int temp = 0;
-                temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+        int vis[][] = new int[m][n]; 
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==j && vis[i][j]==0) vis[i][j]=1;
+                else if (vis[i][j]==0){
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                    vis[i][j] = 1;
+                    vis[j][i] = 1;
+                }
             }
         }
-        // for(int i=0;i<m;i++){
-        //     for(int j = 0;j<n/2;j++){
-        //         int temp =0;
-        //         temp = matrix[i][j];
-        //         matrix[i][j] = matrix[i][n-j-1];
-        //         matrix[i][n-j-1] = temp;
-        //     }
-        // }
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length / 2; j++) {
-                int temp = 0;
-                temp = matrix[i][j];
-                matrix[i][j] = matrix[i][matrix.length - 1 - j];
-                matrix[i][matrix.length - 1 - j] = temp;
-            }
+        for(int it[]: matrix){
+            rev(it);
+        }
+    }
+    public void rev(int[] arr){
+        int ptr1 = 0;
+        int ptr2 = arr.length-1;
+        while(ptr1<ptr2){
+            int temp = arr[ptr1];
+            arr[ptr1] = arr[ptr2];
+            arr[ptr2] = temp;
+            ptr1++;
+            ptr2--;
         }
     }
 }
