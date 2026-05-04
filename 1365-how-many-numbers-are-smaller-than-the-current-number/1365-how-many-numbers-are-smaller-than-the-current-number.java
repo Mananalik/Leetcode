81edceddfd0df818b816c3eac9448f56674c1f98
Manan Malik <135524466+Mananalik@users.noreply.github.com>
2026-05-04 18:34:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,14 +1,20 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int n = nums.length;
-        int ans[] = new int[n];
+        int freq[] = new int [101];
+        int ans[] = new int [n];
+        for(int it: nums ){
+            freq[it]++;
+        }
+        // Calculate prefix sum :
+        for(int i=1;i<101;i++){
+            freq[i] = freq[i]+freq[i-1];
+        }
         for(int i=0;i<n;i++){
-            int num = nums[i];
-            int cnt = 0;
-            for(int j=0;j<n;j++){
-                if(i!=j && nums[j]<num) cnt++;
+            if(nums[i]==0) ans[i] = 0;
+            else{
+                ans[i] = freq[nums[i]-1];
             }
-            ans[i] = cnt;
         }
         return ans;
     }
