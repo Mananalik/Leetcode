e62f204ab9b7821214f42fe84b30c7e4b5743611
Manan Malik <135524466+Mananalik@users.noreply.github.com>
2026-05-07 13:23:22 +0530

    Time: 7 ms (92.6%), Space: 124.9 MB (43.44%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.java
+++ 1861-rotating-the-box/1861-rotating-the-box.java
@@ -2,34 +2,27 @@ class Solution {
     public char[][] rotateTheBox(char[][] box) {
         int m = box.length;
         int n = box[0].length;
-
-        // STEP 1: Apply gravity (move stones to the right)
-        for (int i = 0; i < m; i++) {
-            int emptyPos = n - 1;
-
-            for (int j = n - 1; j >= 0; j--) {
-                if (box[i][j] == '*') {
-                    emptyPos = j - 1;
-                } 
-                else if (box[i][j] == '#') {
-                    if (j != emptyPos) {
-                        box[i][emptyPos] = '#';
+        for(int i=0;i<m;i++){
+            int empty = n-1;
+            for(int j=n-1;j>=0;j--){
+                if(box[i][j]=='*'){
+                    empty  = j-1;
+                }
+                else if(box[i][j]=='#'){
+                    if(j!=empty){
+                        box[i][empty] = '#';
                         box[i][j] = '.';
                     }
-                    emptyPos--;
+                    empty--;
                 }
             }
         }
-
-        // STEP 2: Rotate matrix 90° clockwise
-        char[][] result = new char[n][m];
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                result[j][m - 1 - i] = box[i][j];
+        char[][] ans = new char[n][m];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                ans[j][m-1-i] = box[i][j];
             }
         }
-
-        return result;
+        return ans;
     }
 }
